fd69e40c4d692da7912f9f6cd341e6a3f12ac7c5
sfcourseraR <patriciaogbinar@Patricias-MacBook-Pro.local>
2014-05-25 10:21:36 -0700

    Updated cachematrix.R
    updated cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using the example code provided, set up makeCacheMatrix and cacheSolve.
+## makeCacheMatrix is set up as a matrix (x = matrix()) and replace "mean" with "solve" 
+## for cacheSolve, replace all instances of "mean" with "solve" as well
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+  m <- x$getsolve()
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+## x <- makeCacheMatrix(matrix(c(1:4), nrow = 2, ncol=2))
+## cacheSolve(x) where inverse is calculated and given 
+## cacheSolve (x) where inverse is printed but with "getting cached data" this time
